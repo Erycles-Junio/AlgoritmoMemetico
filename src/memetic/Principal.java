bfac82504fbde5ae59fcd1f174c374e524e052bf
Erycles <erycles-junio@hotmail.com>
2018-03-11 15:45:58 -0300

    Algoritmo memetico - TP IA

---
+++ src/memetic/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memetic;
+
+/**
+ *
+ * @author user
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+        Memetic memetic =  new Memetic();
+        memetic.execute();
+    } 
+    
+}
